f02811a004d41e71219f854afef47bc2b566c8db
David Fowler <davidfowl@gmail.com>
2024-05-19 23:27:57 -0700

    Build bootstrap resource into addeventgridwebhook

--- EventGridDemo.AppHost/EventGridWebHookResource.cs
+++ EventGridDemo.AppHost/EventGridWebHookResource.cs
@@ -4,31 +4,37 @@ public class EventGridWebHookResource(string name) : AzureBicepResource(name, te
 {
     public BicepOutputReference Endpoint => new("endpoint", this);
 
-    public ReferenceExpression ConnectionStringExpression => 
+    public ReferenceExpression ConnectionStringExpression =>
         ReferenceExpression.Create($"{Endpoint}");
+}
+
+internal class EventGridWebHookBootstrapResource(string name) : AzureBicepResource(name, templateFile: "eventgridwebhookbootstrap.bicep")
+{
 }
 
 public static class EventGridExtensions
 {
-    public static IResourceBuilder<EventGridWebHookResource> AddEventGridWebHook(this IDistributedApplicationBuilder builder, string name)
+    public static IResourceBuilder<EventGridWebHookResource> AddEventGridWebHook(this IDistributedApplicationBuilder builder, string name, EndpointReference endpointReference, string webhookPath)
     {
         builder.AddAzureProvisioning();
 
-        var resource = new EventGridWebHookResource(name);
-        return builder.AddResource(resource)
+        // This is a placeholder resource that will be used to validate the EventGrid subscription
+        // event grid validates the webhook by sending a validation event to the webhook during
+        // deployment.
+        var eventgridBootstrapResource = new EventGridWebHookBootstrapResource($"{name}-bootstrap");
+        builder.AddResource(eventgridBootstrapResource)
+               .WithParameter("webhookPath", webhookPath)
+               .WithParameter("name", endpointReference.Resource.Name)
+               .WithParameter("containerAppEnvironmentId")
+               .WithManifestPublishingCallback(eventgridBootstrapResource.WriteToManifest);
+
+        var eventGridWebHookResource = new EventGridWebHookResource(name);
+
+        return builder.AddResource(eventGridWebHookResource)
                       .WithParameter("topicName", name.ToLowerInvariant())
                       .WithParameter(AzureBicepResource.KnownParameters.PrincipalId)
                       .WithParameter(AzureBicepResource.KnownParameters.PrincipalType)
-                      .WithManifestPublishingCallback(resource.WriteToManifest);
-    }
-
-    public static IResourceBuilder<EventGridWebHookResource> WithWebhookUrl(this IResourceBuilder<EventGridWebHookResource> builder, ReferenceExpression webhookExpression)
-    {
-        return builder.WithParameter("webHookEndpoint", () => webhookExpression);
-    }
-
-    public static IResourceBuilder<EventGridWebHookResource> WithWebhookUrl(this IResourceBuilder<EventGridWebHookResource> builder, ReferenceExpression.ExpressionInterpolatedStringHandler webhookExpression)
-    {
-        return builder.WithWebhookUrl(ReferenceExpression.Create(webhookExpression));
+                      .WithParameter("webHookEndpoint", () => ReferenceExpression.Create($"{endpointReference}/{webhookPath}"))
+                      .WithManifestPublishingCallback(eventGridWebHookResource.WriteToManifest);
     }
 }
--- EventGridDemo.AppHost/Program.cs
+++ EventGridDemo.AppHost/Program.cs
@@ -3,8 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.EventGridDemo_Api>("api")
                      .WithExternalHttpEndpoints();
 
-var eventGrid = builder.AddEventGridWebHook("grid")
-                       .WithWebhookUrl($"{api.GetEndpoint("https")}/hook");
+var eventGrid = builder.AddEventGridWebHook("grid", api.GetEndpoint("https"), "hook");
 
 builder.AddProject<Projects.EventGridDemo_Publisher>("publisher")
        .WithReference(eventGrid);
